192c6284313266a73b398bca5e7a8a3e497447af
Vivek Kumar Verma <12vivek94@gmail.com>
2025-02-13 03:06:28 +0530

    customnode component added and removed unnecessary code

--- src/nodes/types.ts
+++ src/nodes/types.ts
@@ -1,7 +1,7 @@
 import type { Node, BuiltInNode } from '@xyflow/react';
 
-export type TaskInNode = Node<{ label: string, taskName: string, assignee: string, date: string }, 'task'>;
-export type ConditionInNode = Node<{ label: string }, 'condition'>;
-export type NotificationInNode = Node<{ label: string }, 'notification'>;
+export type TaskInNode = Node<{ nodeID: string, nodeType: string, nodename: string, assignee: string, duedate: string }, 'task'>;
+export type ConditionInNode = Node<{ nodeID: string, nodeType: string, nodename: string, assignee: string, duedate: string }, 'condition'>;
+export type NotificationInNode = Node<{ nodeID: string, nodeType: string, nodename: string, assignee: string, duedate: string }, 'notification'>;
 export type NodeCreatorInNode = Node<{ label?: string }, 'nodecreator'>;
 export type AppNode = BuiltInNode | TaskInNode | ConditionInNode | NotificationInNode | NodeCreatorInNode;
